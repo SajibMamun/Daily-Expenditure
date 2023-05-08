2e36134adfc33cca2b7264efd4653b193065e3e3
Sajib Bin Mamun <sajibmamun138@gmail.com>
2023-05-08 08:08:36 +0600

    radio btn data retrieve and firebase object initialize

--- app/src/main/java/com/learnwithsajib/dailyexpenditure/RegistrationActivity.kt
+++ app/src/main/java/com/learnwithsajib/dailyexpenditure/RegistrationActivity.kt
@@ -8,17 +8,31 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.learnwithsajib.dailyexpenditure.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity : AppCompatActivity() {
     lateinit var binding: ActivityRegistrationBinding
     lateinit var spinner: Spinner
     lateinit var joinas:String
+
+    lateinit var firebaseAuth: FirebaseAuth
+    lateinit var myref:DatabaseReference
+    lateinit var firebaseUser: FirebaseUser
+    lateinit var uid:String
+    var gender:String?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        //spinner
         val joinaslist=resources.getStringArray(R.array.JoinAs)
         spinner=binding.spinnerid
         if(spinner!=null)
@@ -45,10 +59,30 @@ class RegistrationActivity : AppCompatActivity() {
         }
 
 
+        firebaseAuth=FirebaseAuth.getInstance()
+
+        val database=Firebase.database
+        myref=database.reference.child("User")
+
+        binding.malebtn.setOnClickListener {
+            gender="Male"
+            Toast.makeText(applicationContext,"$gender",Toast.LENGTH_SHORT).show()
+        }
+        binding.femalebtn.setOnClickListener {
+            gender="Female"
+            Toast.makeText(applicationContext,"$gender",Toast.LENGTH_SHORT).show()
+        }
+
+
+
+
+
         binding.SignInTv.setOnClickListener {
             var intent = Intent(applicationContext, SignInActivity::class.java)
             startActivity(intent)
         }
+
+
 
 
     }
